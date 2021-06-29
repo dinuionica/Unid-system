2e93e819fc7c65ece3775174e4b6cd519338f336
Dinu Ion Irinel <51510817+dinuionica@users.noreply.github.com>
2021-06-29 10:38:42 +0300

    Update unid_system.c

--- unid_system.c
+++ unid_system.c
@@ -1,6 +1,8 @@
-/* Copyright 2021 Dinu Ion Irinel */
+/* MIT License
 
-/* Permission is hereby granted, free of charge, to any person obtaining a copy
+   Copyright 2021 Dinu Ion Irinel 
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
    of this software and associated documentation files (the "Software"), to deal
    in the Software without restriction, including without limitation the rights 
    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
