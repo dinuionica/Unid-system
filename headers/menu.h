25aaa8bb6af02aebce4438cf4c5837f10fea2bef
Dinu Ion Irinel <51510817+dinuionica@users.noreply.github.com>
2021-08-02 15:37:09 -0400

    Update menu.h

--- headers/menu.h
+++ headers/menu.h
@@ -1,12 +1,11 @@
-//Copyright 2021 Dinu Ion Irinel
+/* Copyright 2021 Dinu Ion Irinel */
 #ifndef MENU_H
 #define MENU_H_
 
-// function to display menus with all options
 void display_menu();
 
 void commands_menu();
 
 void parse_command();
 
-#endif
+#endif // MENU_H_
